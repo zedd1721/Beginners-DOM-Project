be11cb2bb453500abbafa393a7d332d86311d7d4
Zain Shaikh <szain2024@gmail.com>
2024-01-06 21:37:24 +0530

    New Project

---
+++ 6 - UnlimitedColors/script.js
@@ -0,0 +1,26 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+
+
+
+let myInterval;
+function randomColors(){
+    let randomValue = "0123456789ABCDEF";
+    let colors ="#";
+    for(let i =0; i<6; i++){
+        colors = colors + randomValue[parseInt(Math.random()*16)];
+    }
+    console.log(colors);
+    document.body.style.backgroundColor = colors;
+    
+};
+
+start.addEventListener('click', function(){
+    console.log('Start Button Clicked');
+    myInterval = setInterval(randomColors, 1000);
+});
+
+stop.addEventListener('click', function(){
+    console.log('Stop button clicked');
+    clearInterval(myInterval);
+});
